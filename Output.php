dabe346c5032f15627ebf3ba608ab6f138ba37ad
JoedJoed24 <abartristanjoed_bsit@plmun.edu.ph>
2024-08-28 17:48:47 -0700

    Add files via upload

---
+++ Output.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your Email Address is: <?php echo $_POST["email"]; ?><br>
+Your Address is: <?php echo $_POST["address"]; ?><br>
+Your Phone Number is: <?php echo $_POST["phone_number"]; ?><br>
+Your Gender is:  <?php echo $_POST["gender"]; ?><br>
+Your Father Name is:  <?php echo $_POST["Father_Name"]; ?><br>
+Your Mother Name is:  <?php echo $_POST["Mother_Name"]; ?><br>
+</body>
+</html>
